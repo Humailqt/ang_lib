26e67ab457e64a65e75309997fd3a93ee68809db
Humailqt <Humailqt@gmail.com>
2023-03-03 11:59:12 +0300

    Ветка подготовки к пилоту: Фикс имени файла

--- Studs3d1/studs3d.h
+++ Studs3d1/studs3d.h
@@ -251,7 +251,7 @@ public :
   ////////////////////////////////////////////////////////////////////////////
   int load_default_panel();
   void show_info();
-  std::wstring get_tmp_filename_tmp(IDocument3DPtr doc);
+  CString  get_tmp_filename_tmp(IDocument3DPtr doc);
   InsertPartPtr get_part_info() { return partInfo; }
   bool save_part_info(IPartPtr part, IDocument3DPtr doc, CString patch_file);
 
